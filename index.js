9ea65ae5e513ba818670e4f3ce78cfbd5accf440
Matias Parra <parramatias96@gmail.com>
2025-01-22 19:06:29 -0300

    feat: Get a home route

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.get('/home', (req, res) => {
+    res.send('Hola Mundo');
+    
+  });app.listen(port, () => console.log(`Server running on port ${port}`));

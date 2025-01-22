73552bf96b918895a0dda40790f1d8acafecedb0
Matias Parra <parramatias96@gmail.com>
2025-01-22 20:24:45 -0300

    feat: Change vercel and package json

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-app.get('/home', (req, res) => {
-    res.send('Hola Mundo');
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'The service is up and running'
+    });
     
   });app.listen(port, () => console.log(`Server running on port ${port}`));

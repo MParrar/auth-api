c219ad225041e6e52b2d89060e8914e008bba55f
Matias Parra <parramatias96@gmail.com>
2025-01-22 23:27:48 -0300

    feat: Use neon db connection

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.NODE_ENV === 'test' ? process.env.PG_TEST_USER : process.env.PG_USER,
-  host: process.env.NODE_ENV === 'test' ? process.env.PG_TEST_HOST : process.env.PG_HOST,
-  database: process.env.NODE_ENV === 'test' ? process.env.PG_TEST_DATABASE : process.env.PG_DATABASE,
-  password:  process.env.NODE_ENV === 'test' ? process.env.PG_TEST_PASSWORD : process.env.PG_PASSWORD,
-  port:  process.env.NODE_ENV === 'test' ? process.env.PG_TEST_PORT : process.env.PG_PORT,
-});
+  connectionString: process.env.NODE_ENV === "test" ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL,
+  });
 
 module.exports = pool;

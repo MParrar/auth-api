8d2877c2f7083be859f6efe275ea6359833e8280
Matias Parra <parramatias96@gmail.com>
2025-01-24 16:42:14 -0300

    feat: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config} */
 export default {
-  globals: {
-    process: 'readonly',
-    ...globals.browser,
-    sourceType: 'module', 
-  },
-  extends: [
-    pluginJs.configs.recommended,
-  ],
-  rules: {
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-    'semi': ['error', 'always'],
-    'quotes': ['error', 'single'],
+  languageOptions: {
+    globals: {
+      process: "readonly",
+      ...globals.browser,
+      sourceType: "module",
+    },
+    extends: [pluginJs.configs.recommended],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+    },
   },
 };

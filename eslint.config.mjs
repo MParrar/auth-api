687f0dcb08a36f15a15cb701c1f101fafa3b22b3
Matias Parra <parramatias96@gmail.com>
2025-01-24 16:34:40 -0300

    feat: Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import pluginJs from '@eslint/js';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ['**/*.js'], languageOptions: {sourceType: 'commonjs'}},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: globals.browser, process: 'readonly' }},
   pluginJs.configs.recommended,
   {rules: {
     'no-unused-vars': 'warn',
